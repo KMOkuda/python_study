f0b6902635ac41fe040b3878e9b897ab2b07add4
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2022-11-28 14:05:53 +0900

    rangeとリスト（定義と取り出し）をテスト

---
+++ helloPy/helloPack/range_test.py
@@ -0,0 +1,16 @@
+'''
+Created on 2022/11/04
+
+@author: User105
+
+'''
+
+nums = [1, 2, 3, 4, 5]
+
+print("長さ：" + str(len(nums)))
+
+for i in nums:
+    print(i)
+    
+for i in range(5, 15):
+    print(i)
