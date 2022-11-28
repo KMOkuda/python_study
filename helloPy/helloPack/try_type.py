a38fede1e460eb767d708b1d0e41da73e19a3fc8
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2022-11-28 13:02:41 +0900

    タイプ型と総合演習。

---
+++ helloPy/helloPack/try_type.py
@@ -0,0 +1,13 @@
+'''
+Created on 2022/11/04
+
+@author: User105
+
+型を試してみる
+'''
+
+a = type(int(30.0));
+
+#aはtype型
+
+print(a);
---
+++ helloPy/helloPack/unit_conversion.py
@@ -0,0 +1,14 @@
+'''
+Created on 2022/11/04
+
+@author: User105
+
+今までのおさらい
+'''
+weight = input("体重は？(kg)：");
+
+g = int(float(weight) * 1000);
+
+#aはtype型
+
+print("あなたの体重は" + str(g) + "gです。");
